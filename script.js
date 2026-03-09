1170dbe1bde358d293011a007aeea796e0b62112
thanhtamblog <mrtuan.tran79@gmail.com>
2026-03-09 14:56:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// rút gọn số theo thần số học
 function reduce(num){
 
 while(num > 9 && num !== 11 && num !== 22 && num !== 33){
 
-num = num.toString().split('').reduce((a,b)=>a+parseInt(b),0)
+num = num.toString().split('').reduce((a,b)=>a+Number(b),0)
 
 }
 
@@ -11,6 +12,7 @@ return num
 }
 
 
+// LIFE PATH
 function lifePath(date){
 
 let nums = date.replaceAll("-","").split("").map(Number)
@@ -22,6 +24,7 @@ return reduce(sum)
 }
 
 
+// BIRTHDAY NUMBER
 function birthday(date){
 
 let day = parseInt(date.split("-")[2])
@@ -31,17 +34,19 @@ return reduce(day)
 }
 
 
+// ATTITUDE NUMBER
 function attitude(date){
 
 let parts = date.split("-")
 
-let sum = parseInt(parts[1]) + parseInt(parts[2])
+let sum = Number(parts[1]) + Number(parts[2])
 
 return reduce(sum)
 
 }
 
 
+// MATURITY NUMBER
 function maturity(life,expression){
 
 return reduce(life + expression)
@@ -49,6 +54,7 @@ return reduce(life + expression)
 }
 
 
+// CHUYỂN CHỮ → SỐ
 const letterMap = {
 A:1,B:2,C:3,D:4,E:5,F:6,G:7,H:8,I:9,
 J:1,K:2,L:3,M:4,N:5,O:6,P:7,Q:8,R:9,
@@ -56,15 +62,31 @@ S:1,T:2,U:3,V:4,W:5,X:6,Y:7,Z:8
 }
 
 
+// chuẩn hóa tên (bỏ dấu tiếng Việt)
+function normalizeName(name){
+
+return name
+.normalize("NFD")
+.replace(/[\u0300-\u036f]/g,"")
+.toUpperCase()
+
+}
+
+
+// EXPRESSION NUMBER
 function expression(name){
 
-name = name.toUpperCase().replace(/[^A-Z]/g,'')
+name = normalizeName(name).replace(/[^A-Z]/g,'')
 
 let sum = 0
 
 for(let c of name){
 
+if(letterMap[c]){
+
 sum += letterMap[c]
+
+}
 
 }
 
@@ -73,9 +95,10 @@ return reduce(sum)
 }
 
 
+// SOUL URGE
 function soul(name){
 
-name = name.toUpperCase()
+name = normalizeName(name)
 
 let vowels = "AEIOU"
 
@@ -83,7 +106,7 @@ let sum = 0
 
 for(let c of name){
 
-if(vowels.includes(c)){
+if(vowels.includes(c) && letterMap[c]){
 
 sum += letterMap[c]
 
@@ -96,11 +119,20 @@ return reduce(sum)
 }
 
 
+// MAIN FUNCTION
 function calculate(){
 
-let name = document.getElementById("name").value
+let name = document.getElementById("name").value.trim()
 
 let birth = document.getElementById("birth").value
+
+if(!name || !birth){
+
+alert("Vui lòng nhập đầy đủ họ tên và ngày sinh")
+
+return
+
+}
 
 let life = lifePath(birth)
 
